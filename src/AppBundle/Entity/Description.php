dd2fa2f38c844d7b7bff2098496589b24aceaa06
jschwartzenberg <julius.schwartzenberg@gmail.com>
2016-05-24 17:30:09 +0200

    Description as separate entity Working on the form

--- src/AppBundle/Entity/Description.php
+++ src/AppBundle/Entity/Description.php
@@ -1,6 +1,8 @@
 <?php
 namespace AppBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
 /**
  * @ORM\Entity
  * @ORM\Table(name="description")
@@ -19,5 +21,37 @@ class Description
      * @ORM\Column(type="text")
      */
     private $text;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * @param mixed $text
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+    }
 
 }
---
+++ src/AppBundle/Form/EventType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use AppBundle\Entity\Description;
+
+/**
+ * Created by PhpStorm.
+ * User: jschwartzenberg@soleranl.nl
+ * Date: 5/24/16
+ * Time: 4:29 PM
+ */
+class EventType extends AbstractType
+{
+
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('date', DateTime::class)
+            ->add('number', NumberType::class)
+            ->add('description', Description::class)
+            ->add('save', SubmitType::class)
+        ;
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Event'
+        ));
+    }
+}
